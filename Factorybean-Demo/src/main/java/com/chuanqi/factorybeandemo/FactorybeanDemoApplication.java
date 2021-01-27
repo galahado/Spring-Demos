c21de248cecc923b368e359b603e5c44b25c4758
Chuanqi Zhang <zhang.chua@northeastern.edu>
2021-01-27 12:49:15 -0500

    Init

---
+++ Factorybean-Demo/src/main/java/com/chuanqi/factorybeandemo/FactorybeanDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chuanqi.factorybeandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorybeanDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactorybeanDemoApplication.class, args);
+    }
+
+}
---
+++ Factorybean-Demo/src/test/java/com/chuanqi/factorybeandemo/FactorybeanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuanqi.factorybeandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorybeanDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
